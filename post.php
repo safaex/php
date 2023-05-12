c5a29083e3f28f117d0fee6439182101cca2d207
safaex <133200932+safaex@users.noreply.github.com>
2023-05-12 15:38:45 +0200

    Add files via upload

---
+++ post.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form method="POST" action="verwerking.php">
+
+<input type="text" name="name" placeholder="Naam" required> <br>
+
+<input type="text" name="achternaam" placeholder="achternaam" required> <br>
+
+<input type="text" name="leeftijd" placeholder="leeftijd" required> <br>
+
+<input type="text" name="Adres" placeholder="Adres" required> <br>
+
+<input type="email" name="email" placeholder="Email" required> <br>
+
+
+<h1>Het belangrijkste verschil tussen de methode GET en 
+    POST in PHP is dat de GET-methode de informatie verzendt door 
+    ze aan de paginavraag toe te voegen, terwijl de POST-methode informatie
+     via de HTTP-header verzendt.</h1>
+
+
+<input type="submit" value="Verstuur">
+</body>
+</html>
