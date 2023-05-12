fc120cf87143f092d40143e416b25538c2b2f6b6
safaex <133200932+safaex@users.noreply.github.com>
2023-05-12 14:49:45 +0200

    Add files via upload

---
+++ post.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form method="POST" action="verwerking.php">
+
+<input type="text" name="name" placeholder="Naam" required> <br>
+
+<input type="text" name="achternaam" placeholder="achternaam" required> <br>
+
+<input type="text" name="leeftijd" placeholder="leeftijd" required> <br>
+
+<input type="text" name="Adres" placeholder="Adres" required> <br>
+
+<input type="email" name="email" placeholder="Email" required> <br>
+
+
+<input type="submit" value="Verstuur">
+</body>
+</html>
