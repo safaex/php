e8c6ad47f09ca0beecd8a6a2f377221f89f72169
safaex <133200932+safaex@users.noreply.github.com>
2023-05-12 13:42:01 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Welkom op deze pagina</h1>
+    <?php echo "hello world"; ?>
+
+    <br>
+	<?php 
+	$hello = 10;
+	$doeii = 12;
+	echo  $hello+$doeii;
+   ?>
+
+   <br>
+   <?php
+    $hello = 10;
+	$doeii = 12;
+	echo  10*12;
+    ?>
+
+<br>
+<?php
+  $naam="safa";
+  echo "Welkom"  .$naam;  
+  ?>
+
+ <br>
+ <?php
+
+// deze code bevat veel fouten haal alle fouten eruit!
+//
+
+$mijnNaam="Carl";
+$jouwNaam="Camel";
+echo $mijnNaam. " zit bij " . $jouwNaam . " in de klas!"
+
+?>
+
+
+
+
+</body>
+</html>
